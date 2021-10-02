2a140de57e7279111dccccabf3dbb580750a4e94
ootomoakiyoshi <ootomoakiyoshi@icloud.com>
2021-10-02 11:35:24 +0000

    途中

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+
+
+  resources :users,only: [:show,:index,:edit,:update]
+
+  resources :books
+
+ end
